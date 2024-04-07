0895ac0437a136f43322f0c2b9a3ad5b1526c4d4
Austin Higgins <austinjhiggins@pm.me>
2024-04-07 19:23:50 -0230

    Created some boilerplate JavaScript and edited HTML to go along with the js code

--- js/script.js
+++ js/script.js
@@ -1,3 +1,42 @@
 let NasaKey = config.NasaKey;
 
 // use url: link + NasaKey
+
+// Create 12 other objects
+// changing color of planets
+// click and drag
+// 
+
+class Planet {
+    constructor(radius, color, name, description) {
+      this.radius = radius;
+      this.color = color;
+      this.name = name;
+      this.description = description;
+    }
+  }
+
+function testAlert() {
+    alert("Hello! I am an alert box!!");
+}
+
+function convertToScale(distanceValue) {
+    return distanceValue;
+}
+
+function getObjectDataFromNasaApi () {
+    // Check out exoplanet API
+}
+
+function generateBody() {
+
+}
+
+function generateBackground() {
+
+}
+
+function populateBody(body) {
+    document.getElementById("bodyTitle").innerHTML = body.name;
+    document.getElementById("bodyDescription").innerHTML = body.description;
+}
